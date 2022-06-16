f95c5849456bdf10a6bff49ff8aeb0a9375e28e1
leah karienye <karienyeleah@gmail.com>
2022-06-16 15:40:08 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData (name, email){
+  const submission = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    })
+}
+  return fetch("http://localhost:3000/users", submission)
+  
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    return document.body.innerHTML = json.id;
+  })
+  .catch(function (error) {
+    document.body.innerHTML = error.message
+    
+  })
+}
