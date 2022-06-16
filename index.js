d5045417d60fc4e16b4cb8ee7ab15efb49c9ac7f
leah karienye <karienyeleah@gmail.com>
2022-06-16 14:43:31 +0300

    commit tree 2b893dc854a0a0ab7a2d613b5407c2aac87bb678

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const submitData = {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: "shiku",
+      email: "shiku@gmail.com",
+    }),
+  };
+  
+  fetch("http://localhost:3000/users", submitData())
+  
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("error occured");
+    console.log(error.message);
+  });
+  console.log(submitData);
